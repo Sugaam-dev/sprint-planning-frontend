9f843ed43101a86392b010076e54c8a5206de34d
Dhurjoti Ghosh <dhurjotig@gmail.com>
2026-08-21 13:43:11 +0530

    react jsx folder structure

---
+++ src/pages/Landing/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LandingPage = () => {
+  
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+    
+        <p className="mt-4 text-2xl text-gray-600">
+          Welcome to Landing Page
+        </p>
+      </div>
+    </div>
+
+  );
+};
+
+export default LandingPage;
